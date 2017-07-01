552518973228030a4851cd32b231983b57b286f7
hlh <517374135@qq.com>
2017-07-01 12:03:44 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+//引入gulp模块
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//创建gulp任务 以编译sass文件
+gulp.task('compileSass',function(){
+	gulp.src('web/sass/*.scss')
+	//通过pipe方法导入到插件中实现编译sass
+	.pipe(sass({outputStyle:'extended'}).on('error',sass.logError))
+	//把编译的文件输出
+	.pipe(gulp.dest('web/css/'));
+});
+
+//监听文件 自动执行任务
+gulp.task('listenerSass',function(){
+	gulp.watch('web/sass/*.scss',['compileSass']);
+});
+
+//浏览器同步任务
+var browserSync = require('browser-sync');
+gulp.task('autoRefresh',function(){
+	browserSync({
+		//server:'src',
+		//代理服务器
+		proxy:'http://localhost/retail-system/',
+		//port:8080,//自定义端口
+		//监听文件修改，自动刷新浏览器
+		files:['web/html/*.html','web/index.html','src/js/*.js']
+	});
+
+	//监听sass文件修改，执行编译sass文件任务
+	gulp.watch('web/sass/*.scss',['compileSass']);
+});
+
+// 用于js文件的压缩
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+gulp.task('compressJS',function(){
+	gulp.src('web/js/*.js')
+
+	// 合并
+	.pipe(concat('page.js'))
+
+	// 输入合并后但未压缩的版本
+	.pipe(gulp.dest('dist/js/'))
+
+	// 压缩
+	.pipe(uglify({
+		mangle:false,
+		compress:false
+	}))
+
+	// 重命名
+	.pipe(rename({
+		suffix:'.min'
+	}))
+
+	// 输出
+	.pipe(gulp.dest('dist/js/'))
+});
